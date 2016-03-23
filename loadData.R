70d506a72affe4ce5b4fd9285fe08b9770bd6993
rhinophile <bijan.text@gmail.com>
2016-03-23 09:27:45 -0400

    init1

--- loadData.R
+++ loadData.R
@@ -1,1 +1,7 @@
+source('auxFunction.R')
+
 sapfluxBW <- read.csv('data/sapFlux.BW.csv')
+sapfluxParker <- read.csv('data/sapFlux.Parker.csv')
+
+ameriParker <- readAmeriFlux('data/other data/North_Carolina_Loblolly_Pine/with_gaps/')
+ameriBW <- readAmeriFlux('da')
