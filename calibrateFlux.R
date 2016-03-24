06399f7fcf5936b56d1dfa05c6a6ec115b250a29
rhinophile <bijan.text@gmail.com>
2016-03-24 15:52:49 -0400

    calibrate fluxes nc/hw=1.34

--- calibrateFlux.R
+++ calibrateFlux.R
@@ -1,1 +1,19 @@
-readAmeriFlux('')
+hwGap <- readAmeriFlux('ameriflux_data/Duke_Forest_Hardwoods/gap_filled')
+hwFill <- readAmeriFlux('ameriflux_data/Duke_Forest_Hardwoods/with_gaps/')
+ncGap <- readAmeriFlux('ameriflux_data/North_Carolina_Loblolly_Pine/with_gaps/')
+
+plt <- function(data) plot(data$data$YEAR+(data$data$DTIME-1)/366, data$data$LE)
+
+par(mfcol=c(3,1))
+
+library(data.table)
+
+nc <- as.data.table(ncGap$data)
+hw <- as.data.table(hwFill$data)
+
+p.nc <- nc[YEAR%in%c(2005:2008), .(p=sum(PREC, na.rm = T)), YEAR]
+p.hw <- hw[YEAR%in%c(2005:2008), .(p=sum(PREC, na.rm = T)), YEAR]
+
+m.nc <- nc[YEAR%in%c(2005:2008), .(m=mean(LE, na.rm = T)), YEAR]
+m.hw <- hw[YEAR%in%c(2005:2008), .(m=mean(LE, na.rm = T)), YEAR]
+m.nc$m/m.hw$m
