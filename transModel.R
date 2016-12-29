1ebb3fe36939725e0fee4660663e016bc7d940c8
rhinophile <bijan.text@gmail.com>
2016-12-29 11:19:31 -0500

    BW and PK as separate models

--- transModel.R
+++ transModel.R
@@ -3,47 +3,43 @@ library(gjam)
 source('~/Projects/procVisData/dataViz.R')
 
 
-source('~/Projects/transFlux/transPreModel.R')
+# source('~/Projects/transFlux/transPreModel.R')
 
-# transData[Year==2010,TR:=NA]
+xydata <- transData[,
+                    .(Site, 
+                      dT=(LST-TA), 
+                      Solar=Rg, 
+                      Thermal= 0.96*(LST+273)^4 + 0.75*(TA+273)^4,
+                      LST4 = (LST+273)^4,
+                      WS,
+                      PR,
+                      windy = (WS > 2)*1,
+                      rainy = (PR > 0)*1,
+                      cloud = log(Rg/RgOpen),
+                      TR, dEVI, dNDVI, EVI, NDVI)]
 
-xydata <- transData[Site=='BW',.(Site, 
-                                 dT=(LST-TA), 
-                                 Solar=Rg, 
-                                 Thermal= 0.96*(LST+273)^4 + 0.75*(TA+273)^4,
-                                 LST4 = (LST+273)^4,
-                                 Site, 
-                                 WS,
-                                 PR,
-                                 windy = (WS > 2)*1,
-                                 rainy = (PR > 0)*1,
-                                 cloud = log(Rg/RgOpen),
-                                 TR, dEVI, dNDVI, EVI, NDVI)]
-xydata <- na.omit((xydata))
-ydata <- xydata[,c('TR','dEVI', 'dNDVI')]
+xydata.BW <- na.omit((xydata[Site=='BW']))
+ydata.BW <- xydata.BW[,c('TR','dEVI', 'dNDVI')]
 
-str(xydata)
-str(ydata)
-summary(xydata)
-colnames(xydata)
-
+xydata.PK <- na.omit((xydata[Site=='PK']))
+ydata.PK <- xydata.PK[,c('TR','dEVI', 'dNDVI')]
 
 modelList <- list(ng = 2000, burnin = 1000, typeNames = c('CA',rep('CON',2)))
 
-out <- gjamGibbs( ~ (dT + Solar + Thermal)*EVI
-                  # + Site +
-                  #   Site:Solar + Site:Thermal
-                  ,
-                  xdata = xydata, 
-                  ydata = ydata,
+out.BW <- gjamGibbs( ~ dT + Solar + Thermal + I(windy*dT) + I(rainy*dT) + I(cloud*dT),
+                  xdata = xydata.BW, ydata = ydata.BW,
                   modelList = modelList)
 
+out.PK <- gjamGibbs( ~ dT + Solar + Thermal + I(windy*dT) + I(rainy*dT) + I(cloud*dT),
+                     xdata = xydata.PK, ydata = ydata.PK,
+                     modelList = modelList)
+
 # gjamPlot(out, plotPars = list(width=4, height=4, sigOnly=F))
-
-plotGibbsBoxplots(out$chains$bgibbs[1001:2000,c("TR_dT","TR_dT")] )
-
-transSumm <- t(apply(out$chains$bgibbs, quantile, MARGIN = 2, probs=c(.025,.975)))
-transSumm[,sig:=(sign(`2.5%`)+sign(`97.5%`))/2]
-rownames(transSumm) <- colnames(out$chains$bgibbs)
-str(transSumm)
-print(transSumm)
+# 
+# plotGibbsBoxplots(out$chains$bgibbs[1001:2000,c("TR_dT","TR_dT")] )
+# 
+# transSumm <- as.data.table(t(apply(out$chains$bgibbs, quantile, MARGIN = 2, probs=c(.025,.975))))
+# transSumm$sig <- rowMeans(sign(transSumm))
+# rownames(transSumm) <- colnames(out$chains$bgibbs)
+# str(transSumm)
+# print(transSumm)
