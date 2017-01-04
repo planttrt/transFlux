7bcf58c87e88d8c321435c160c770d1748fc26ff
rhinophile <bijan.text@gmail.com>
2017-01-04 15:56:31 -0500

    coloring in windrain

--- figures/transFluxFig.WindRain.R
+++ figures/transFluxFig.WindRain.R
@@ -1,4 +1,8 @@
 source('~/Projects/procVisData/dataViz.R')
+source('~/Projects/procVisData/colorSet.R')
+
+colList <- colList.brownGreyGreen
+
 
 bg <- bothSites$pk$betaStd
 
@@ -9,8 +13,9 @@ noRain <- bg[,"rainy.dT"]+bg[,"dT"]
 windRain <- cbind(wind, noWind, rain, noRain)
 
 png('figures/transFluxFig.WindRain.png', res=150, width = 8, height = 5, units = 'in')
-par(mar=c(4,4,2,1))
+par(mar=c(4,4,3,1))
 par(mfrow=c(1,2))
-boxplot(windRain[,1:2], outline=F, ylim=c(-.6,-.1), names=c('windy', 'not windy'))
-boxplot(windRain[,3:4], outline=F, ylim=c(-.6,-.1), names=c('rainy', 'not rainy'))
+boxplot(windRain[,1:2], outline=F, ylim=c(-.6,-.1), names=c('windy', 'not windy'), col=colList[c(1,3)])
+boxplot(windRain[,3:4], outline=F, ylim=c(-.6,-.1), names=c('rainy', 'not rainy'), col=colList[c(1,3)])
+mtext(text = 'Sensitivity of transpiration to dT', outer = T, line = -2, font=2, cex=1.3)
 dev.off()
